5ed2e83950afff7f5b769f615f707192c3c3cf2d
Jörg Planner <joerg.planner@methodpark.de>
2022-04-01 15:41:02 +0200

    [poker] Add missing Hand variants + structs

--- Language/Rust/holdem_poker/src/main.rs
+++ Language/Rust/holdem_poker/src/main.rs
@@ -7,6 +7,12 @@ enum Hand {
     HighCard(HighCard),
     Pair(Pair),
     TwoPairs(TwoPairs),
+    ThreeOfKind(ThreeOfKind),
+    Straight(Straight),
+    Flush(Flush),
+    FullHouse(FullHouse),
+    FourOfKind(FourOfKind),
+    StraightFlush(StraightFlush),
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
@@ -28,10 +34,59 @@ struct TwoPairs {
     high_card: u8,
 }
 
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct ThreeOfKind {
+    kind: u8,
+    // TODO needed? high_card: u8
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct Straight {
+    end: u8,
+    // TODO needed? high_card: u8
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct Flush {
+    // TODO needed? high_card: u8
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct FullHouse {
+    three_kind: u8,
+    // TODO needed? high_card: u8
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct FourOfKind {
+    kind: u8,
+    // TODO needed? high_card: u8
+}
+
+#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+struct StraightFlush {
+    end: u8,
+    // TODO needed? high_card: u8
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rand::prelude::*;
+
+    #[test]
+    fn high_card_normal() {
+        let winner = Hand::HighCard(HighCard {
+            card: 9,
+            second_card: 2,
+        });
+        let loser = Hand::HighCard(HighCard {
+            card: 8,
+            second_card: 7,
+        });
+
+        assert!(winner > loser);
+    }
 
     #[test]
     fn high_card_tiebreaker_second_high_card() {
@@ -48,6 +103,20 @@ mod tests {
     }
 
     #[test]
+    fn pair_normal() {
+        let winner = Hand::Pair(Pair {
+            pair: 9,
+            high_card: 9,
+        });
+        let loser = Hand::Pair(Pair {
+            pair: 8,
+            high_card: 10,
+        });
+
+        assert!(winner > loser);
+    }
+
+    #[test]
     fn pair_tiebreaker_high_card() {
         let winner = Hand::Pair(Pair {
             pair: 8,
@@ -56,6 +125,22 @@ mod tests {
         let loser = Hand::Pair(Pair {
             pair: 8,
             high_card: 7,
+        });
+
+        assert!(winner > loser);
+    }
+
+    #[test]
+    fn two_pairs_normal() {
+        let winner = Hand::TwoPairs(TwoPairs {
+            high: 9,
+            low: 2,
+            high_card: 9,
+        });
+        let loser = Hand::TwoPairs(TwoPairs {
+            high: 8,
+            low: 7,
+            high_card: 10,
         });
 
         assert!(winner > loser);
@@ -103,6 +188,10 @@ mod tests {
             Hand::HighCard(HighCard {
                 card: 8,
                 second_card: 7,
+            }),
+            Hand::HighCard(HighCard {
+                card: 9,
+                second_card: 2,
             }),
             Hand::Pair(Pair {
                 pair: 8,
