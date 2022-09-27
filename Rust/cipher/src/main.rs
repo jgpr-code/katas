b3ddaecdfc0ba3a8c59dec67b345bdc248a72f2f
Jörg Planner <joerg.planner@methodpark.de>
2022-09-27 09:42:21 +0200

    Restructure cipher implementation

--- Rust/cipher/src/main.rs
+++ Rust/cipher/src/main.rs
@@ -1,40 +1,113 @@
-fn modulus(num: i8, modul: i8) -> i8 {
-    let non_negative = if num < 0 { num + modul } else { num };
-
-    non_negative % modul
+trait Encryption {
+    fn encrypt(&self, msg: &str) -> String;
+    fn decrypt(&self, msg: &str) -> String;
 }
 
-fn shift_letter(c: char, shift: i8) -> char {
-    if !c.is_ascii_alphabetic() {
-        return c;
+// TODO write tests macro
+
+struct AsciiCaesarCipher {
+    offset_lower_a: i8,
+    offset_upper_a: i8,
+    default_shift: i8,
+    alphabet_size: i8,
+}
+
+// TODO can this be moved to the impl block?
+// Right now it is only used by shift_default and shift_default_string
+enum ShiftDirection {
+    FORWARD,
+    BACKWARD,
+}
+
+impl AsciiCaesarCipher {
+    // TODO make less verbose if possible e.g. remove repetition of offsets
+    fn new(default_shift: i8, alphabet_size: i8) -> AsciiCaesarCipher {
+        AsciiCaesarCipher {
+            offset_lower_a: 'a' as i8,
+            offset_upper_a: 'A' as i8,
+            default_shift,
+            alphabet_size,
+        }
+    }
+    fn rot13() -> AsciiCaesarCipher {
+        AsciiCaesarCipher {
+            offset_lower_a: 'a' as i8,
+            offset_upper_a: 'A' as i8,
+            default_shift: 13,
+            alphabet_size: 26,
+        }
+    }
+    fn rot_x(x: i8) -> AsciiCaesarCipher {
+        AsciiCaesarCipher {
+            offset_lower_a: 'a' as i8,
+            offset_upper_a: 'A' as i8,
+            default_shift: x,
+            alphabet_size: 26,
+        }
     }
 
-    let ascii_a: i8 = if c.is_ascii_lowercase() {
-        'a' as i8
-    } else {
-        'A' as i8
-    };
-
-    ((modulus(c as i8 - ascii_a + shift, 26)) + ascii_a) as u8 as char
-}
-
-fn caesar_encrypt(text: &str, shift: i8) -> String {
-    let mut answer = String::new();
-
-    for c in text.chars() {
-        answer.push(shift_letter(c, shift));
+    fn to_offset_char(i: i8, offset: i8) -> char {
+        (i + offset) as u8 as char
     }
 
-    answer
+    fn to_offset_i8(c: char, offset: i8) -> i8 {
+        c as i8 - offset
+    }
+
+    fn shift(&self, c: char, shift: i8) -> char {
+        if !c.is_ascii_alphabetic() {
+            return c;
+        }
+
+        let ascii_offset = if c.is_ascii_lowercase() {
+            self.offset_lower_a
+        } else {
+            self.offset_upper_a
+        };
+
+        let c_i8 = Self::to_offset_i8(c, ascii_offset);
+        let shifted = (c_i8 + shift).rem_euclid(self.alphabet_size);
+
+        Self::to_offset_char(shifted, ascii_offset)
+    }
+
+    fn shift_string(&self, text: &str, shift: i8) -> String {
+        let mut shifted = String::new();
+        for c in text.chars() {
+            shifted.push(self.shift(c, shift));
+        }
+        shifted
+    }
+
+    fn shift_default(&self, c: char, direction: ShiftDirection) -> char {
+        match direction {
+            ShiftDirection::FORWARD => self.shift(c, self.default_shift),
+            ShiftDirection::BACKWARD => self.shift(c, -self.default_shift),
+        }
+    }
+
+    fn shift_default_string(&self, text: &str, direction: ShiftDirection) -> String {
+        match direction {
+            ShiftDirection::FORWARD => self.shift_string(text, self.default_shift),
+            ShiftDirection::BACKWARD => self.shift_string(text, -self.default_shift),
+        }
+    }
 }
 
-fn caesar_decrypt(text: &str, shift: i8) -> String {
-    caesar_encrypt(text, -shift)
+impl Encryption for AsciiCaesarCipher {
+    fn encrypt(&self, msg: &str) -> String {
+        self.shift_default_string(msg, ShiftDirection::BACKWARD)
+    }
+
+    fn decrypt(&self, msg: &str) -> String {
+        self.shift_default_string(msg, ShiftDirection::FORWARD)
+    }
 }
 
 fn main() {
     println!("Hello, world!");
-    let a = caesar_encrypt("Method ,.Park!?", 1);
+    let rot13 = AsciiCaesarCipher::rot13();
+    let a = rot13.encrypt("Method ,.Park!?");
     println!("{}", a);
 }
 
@@ -45,16 +118,18 @@ mod tests {
     #[test]
     fn test() {
         let num: i8 = -2;
-        assert_eq!(modulus(num, 26), 24);
+        assert_eq!(num.rem_euclid(26), 24);
     }
 
     #[test]
     fn caesar_identity() {
         let text = "Method Park";
 
-        let encrypted = caesar_encrypt(text, 13);
+        let rot13 = AsciiCaesarCipher::rot13();
 
-        let decrypted = caesar_decrypt(&encrypted, 13);
+        let encrypted = rot13.encrypt(text);
+
+        let decrypted = rot13.decrypt(&encrypted);
 
         assert_eq!(decrypted, text);
     }
