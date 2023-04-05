8e4d8ce7bc756a1df211831fb6e75857b97643e3
Jörg Planner <joerg.planner@methodpark.de>
2023-04-05 08:42:03 +0200

    Add program outline comment in rose_words.fsx

---
+++ Fsharp/spring_puzzle/rose_words.fsx
@@ -0,0 +1,11 @@
+// create 2D array or list with letters
+
+// filter function that lists all words of length n from 2D array/list (vertical, horizontal, diagonal [8 directions in total])
+
+// for all positions
+// for all directions
+// - go in dir until n elements are reached
+// - if not enough -> None
+// - else n elements
+
+// filter words that contain letter at specific position
