6627e1a1e62f5bedc5636fbf74d5201049c1c9f0
Jörg Planner <joerg.planner@methodpark.de>
2024-03-15 10:57:40 +0100

    [minesweeper] setup + first test

---
+++ rust/minesweeper/src/main.rs
@@ -0,0 +1,55 @@
+struct MinesField {
+    rows: usize,
+    cols: usize,
+    data: Vec<char>,
+}
+
+impl From<&str> for MinesField {
+    // a field looks like this
+    // 1 1
+    // *
+    // or this
+    // 3 4
+    // *...
+    // ..*.
+    // ....
+    fn from(field_str: &str) -> Self {
+        let mut lines = field_str.lines();
+        let (rows, cols) = lines
+            .next()
+            .unwrap()
+            .split_once(" ")
+            .map(|(a, b)| (a.parse().unwrap(), b.parse().unwrap()))
+            .unwrap();
+        let mut data = Vec::new();
+        for line in lines {
+            let mut chars: Vec<char> = line.chars().collect();
+            assert!(chars.len() == cols);
+            data.append(&mut chars);
+        }
+        MinesField { rows, cols, data }
+    }
+}
+
+impl MinesField {
+    fn hint_field(&self) -> String {
+        String::from("0\n")
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod test {
+    use crate::MinesField;
+
+    #[test]
+    fn field_1x1_no_mines() {
+        let input = "1 1\n.\n";
+        let expected = "0\n";
+        let field = MinesField::from(input);
+        assert_eq!(expected, field.hint_field().as_str());
+    }
+}
